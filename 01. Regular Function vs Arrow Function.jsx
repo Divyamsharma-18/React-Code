afde7881e2410f5ae198be3a418df98c744eee91
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-03-22 03:55:19 +0530

    Create 01. Regular Function vs Arrow Function.jsx

---
+++ 01. Regular Function vs Arrow Function.jsx
@@ -0,0 +1,9 @@
+// Regular Functions are normal functions used for declaration of functions
+
+function Eat(){
+  return <p>I eat apples.</p>;
+}
+export default Eat;
+
+
+// Arrow functions can be used directly with variables like -
