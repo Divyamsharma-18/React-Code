38dfedb171331d8d4ba0146fc5edbcfd1f73f88d
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-03-23 01:49:51 +0530

    Create 04. Props Drilling.jsx

---
+++ 04. Props Drilling.jsx
@@ -0,0 +1,7 @@
+// Passing data from Parent Component to a deeply nested child component through multiple intermediate components! 
+
+function App (){
+
+
+
+// Redux is used for this, to efficiently use 
