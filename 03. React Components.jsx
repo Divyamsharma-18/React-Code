0c4ed9e983989172ecf6aedc8e0cd593437f7f07
Divyam Sharma <122789885+Divyamsharma-18@users.noreply.github.com>
2025-03-23 01:24:45 +0530

    Create 03. React Components.jsx

---
+++ 03. React Components.jsx
@@ -0,0 +1,15 @@
+// Import the react
+import React from 'react';
+
+// Functional Component
+function Component () {
+  // JSX is written for the UI 
+return (
+  <>
+     <p>Hey, I'm a component.</p>
+  </>
+  )
+}
+
+// export the functional component for further use
+export default Component;
